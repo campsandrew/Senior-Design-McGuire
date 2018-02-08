bdca4a81c056c14fc00f6c1bf0dc8b71e0a947f0
andrewcamps <andrewcamps@email.arizona.edu>
2018-02-08 04:41:26 -0700

    fixed

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,10 @@
 var express = require('express');
-<<<<<<< HEAD
 var path = require("path");
-=======
-var path = require('path');
->>>>>>> develop
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
   res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
-=======
-  res.sendFile(path.join(__dirname, '../', 'views/', 'index.html'));
->>>>>>> develop
 });
 
 module.exports = router;
